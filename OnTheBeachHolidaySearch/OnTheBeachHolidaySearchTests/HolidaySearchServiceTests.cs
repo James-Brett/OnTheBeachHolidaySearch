fbac9f4de7eb0f13c9b34e2bfcb213e44990b9f1
James Brett <jamesbrett3232@gmail.com>
2022-04-23 22:43:27 +0100

    added further flight import test

--- OnTheBeachHolidaySearch/OnTheBeachHolidaySearchTests/HolidaySearchServiceTests.cs
+++ OnTheBeachHolidaySearch/OnTheBeachHolidaySearchTests/HolidaySearchServiceTests.cs
@@ -11,5 +11,18 @@ namespace OnTheBeachHolidaySearchTests
             var flights = HolidaySearchService.ImportFlights();
             Assert.NotEmpty(flights);
         }
+
+        [Fact]
+        public void ImportFlight_HasCorrectValueForItem()
+        {
+            var flights = HolidaySearchService.ImportFlights();
+            var flight = flights[0];
+            Assert.Equal(1, flight.Id);
+            Assert.Equal("First Class Air", flight.Airline);
+            Assert.Equal("MAN", flight.From);
+            Assert.Equal("TFS", flight.To);
+            Assert.Equal(470, flight.Price);
+            Assert.Equal("2023-07-01", $"{flight.DepartureDate:yyyyy-MM-dd}");
+        }
     }
 }
